0a5a78d094e5078ce4879e5603aa6f48aae7dcc3
Borjan M <119819015+BorjanM@users.noreply.github.com>
2023-03-20 12:30:43 +0100

    Add files via upload

---
+++ HomeWork 16.03.2023 Class 05/HomeWork 16.03.2023 Class 05/Program.cs
@@ -0,0 +1,51 @@
+﻿////Task 1
+
+Console.WriteLine("Enter your string");
+string fiveCharacters = Console.ReadLine();
+string lastFiveChars = fiveCharacters.Substring(fiveCharacters.Length - 5);
+Console.WriteLine(lastFiveChars);
+
+//Task 2
+Console.WriteLine("Enter your sentence");
+string sentence = Console.ReadLine();
+string[] splitSentence = sentence.Split(' ');
+foreach (string partOfSentence in splitSentence)
+{
+    Console.WriteLine(partOfSentence);
+}
+
+
+//Task 3
+
+int calculateSum(int number)
+{
+    int sumOfNumbers = 0;
+    while (number != 0)
+    {
+        sumOfNumbers += number % 10;
+        number /= 10;
+    }
+    return sumOfNumbers;
+}
+int resultOfNumbers = calculateSum(55);
+Console.WriteLine(resultOfNumbers);
+
+
+
+//Task 4
+
+int AgeCalculator(DateTime birthDate)
+{
+    DateTime now = DateTime.Now;
+    int age = now.Year - birthDate.Year;
+    if (now.Month < birthDate.Month || (now.Month == birthDate.Month && now.Day < birthDate.Day))
+    {
+        age--;
+    }
+    return age;
+}
+
+DateTime myBirthDate = new DateTime(1992, 04, 19);
+int myAge = AgeCalculator(myBirthDate);
+Console.WriteLine(myAge);
+//poslednava ja resiv so mala pomos na google da si priznaam :) 
